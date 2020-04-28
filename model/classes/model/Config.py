8560c580689602d1136ec301a9e596afe0c9a21e
kevinkoessl <kevin.koessl@gmail.com>
2020-04-28 17:15:04 +0200

    Final Adjustments for handout.

--- model/classes/model/Config.py
+++ model/classes/model/Config.py
@@ -1,7 +1,8 @@
 __author__ = 'Tony Beltramelli - www.tonybeltramelli.com'
+__modified = 'Kevin Kössl'
 
 CONTEXT_LENGTH = 48
-IMAGE_SIZE = 256
+IMAGE_SIZE = 160
 BATCH_SIZE = 64
 EPOCHS = 10
 STEPS_PER_EPOCH = 72000
